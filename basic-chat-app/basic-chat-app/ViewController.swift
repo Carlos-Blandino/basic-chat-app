abc151d86687c6cf51069f05db281c1debcc6d83
Carlos Blandino <cablandino66@gmail.com>
2020-06-10 21:40:22 -0700

    First project commit

---
+++ basic-chat-app/basic-chat-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basic-chat-app
+//
+//  Created by Carlos Blandino on 6/10/20.
+//  Copyright © 2020 Carlos Blandino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
