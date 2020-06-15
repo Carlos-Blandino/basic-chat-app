62b9ce14a58ba7194164f5055d5c017ed102efba
Carlos Blandino <cablandino66@gmail.com>
2020-06-14 18:46:01 -0700

    working on log in and error alert

--- basic-chat-app/basic-chat-app/Controllers/LoginViewController.swift
+++ basic-chat-app/basic-chat-app/Controllers/LoginViewController.swift
@@ -8,6 +8,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -16,6 +17,20 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                if let errors = error?.localizedDescription {
+                    let alert = UIAlertController(title: "Error", message: errors, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (uiAlertAction) in
+                        //print(errors)
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChatSegue", sender: self)
+                }
+            }
+        }
+        
     }
     
 }
