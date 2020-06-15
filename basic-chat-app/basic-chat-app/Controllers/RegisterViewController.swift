41d0eafd1b9d217a5d6609af7b00551348e20cb0
Carlos Blandino <cablandino66@gmail.com>
2020-06-14 18:18:12 -0700

    working on register error notification and authentication

--- basic-chat-app/basic-chat-app/Controllers/RegisterViewController.swift
+++ basic-chat-app/basic-chat-app/Controllers/RegisterViewController.swift
@@ -8,13 +8,37 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-    
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                if let errors = error {
+                    let myError = "\(errors)"
+                    let startingIndex =  (myError.firstIndex(of: "T")!)
+                
+                    let substring = "\(myError[startingIndex...myError.firstIndex(of: ".")!])"
+                    let alert = UIAlertController(title: "Error",
+                                                  message: substring,
+                    preferredStyle: UIAlertController.Style.alert)
+                    
+                   alert.addAction(UIAlertAction(title: "Okay",
+                                                 style: UIAlertAction.Style.default,
+                    handler: {(alert: UIAlertAction!) in print(errors)}))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChatSegue", sender: self)
+                }
+            }
+        }
+        
+        
     }
     
 }
